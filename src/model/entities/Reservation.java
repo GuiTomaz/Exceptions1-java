3e2daa639db81d764bdef358e26c75b7bb444c20
GuiTomaz <lhermetomaz@gmail.com>
2022-10-08 16:38:46 -0300

    Good solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,14 +4,22 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
 	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
 		super();
+		Date now = new Date();
+		if(checkIn.before(now) || checkOut.before(now)){
+			throw new DomainException( "Reservation dates for update must be future dates");
+		}else if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -36,16 +44,18 @@ public class Reservation {
 		
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) throws DomainException {
+		//Adicionar o throws significa que o método PODE lançar/propagar uma exceção. Caso contrário, se somos obrigados a corrrigir
+		//a exceção, então teríamos de faze-lo nesse método. Mas, podemos propagar! :)
 		Date now = new Date();
 		if(checkIn.before(now) || checkOut.before(now)){
-			return "Reservation dates for update must be future dates";
+			throw new DomainException( "Reservation dates for update must be future dates");
 		}else if (!checkOut.after(checkIn)) {
-			return "Check-out date must be after check-in date";
+			throw new DomainException("Check-out date must be after check-in date");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
+		
 	}
 	
 	@Override
